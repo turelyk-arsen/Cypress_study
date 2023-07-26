86cbd207e39119d900c8612cec5e7b1af6fb1ed9
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-07-26 17:39:21 +0200

    report mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      this.screenshotOnRunFailure=true;
       // implement node event listeners here
     },
     // baseUrl: 'http://localhost:8080',
